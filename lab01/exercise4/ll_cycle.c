947c31f8787e1c94303b3cb0a1e3850ade7e4ac5
Ayesha Qadir <ayeshaqadir2252005@gmail.com>
2025-06-13 14:43:15 +0500

    exercise3 code completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,18 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if (head == NULL) return 0;
+
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        slow_ptr = slow_ptr->next;               
+        fast_ptr = fast_ptr->next->next;        
+        if (slow_ptr == fast_ptr) {
+            return 1;  // Cycle detected
+        }
+    }
+
+    return 0;  // Reached end, no cycle
 }
